d9d51c87e5429411cfaeab0c37e3cb267b8ed687
vijay810 <vijay951mane@gmail.com>
2026-01-08 15:26:51 +0530

    update

--- api/index.js
+++ api/index.js
@@ -167,43 +167,11 @@
 
 
 
-require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
-const connectDB = require('../config/db');
-const errorHandler = require('../middlewares/error.middleware');
-
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
-// Test route
 app.get('/', (req, res) => {
-   res.send('Server is working!');
+   res.status(200).send('Vercel API is working');
 });
 
-// Routes
-app.use('/auth', require('../routes/auth.routes'));
-app.use('/leave', require('../routes/leave.routes'));
-app.use('/clients', require('../routes/clients.routes'));
-app.use('/user', require('../routes/user.routes'));
-app.use('/news', require('../routes/news.routes'));
-
-// Error handler
-app.use(errorHandler);
-
-// Connect DB once (important for Vercel)
-let isConnected = false;
-
-async function startServer() {
-   if (!isConnected) {
-      await connectDB(process.env.MONGO_URL);
-      isConnected = true;
-   }
-}
-
-startServer();
-
-// ⬇️ EXPORT EXPRESS APP (NO serverless-http)
 module.exports = app;
