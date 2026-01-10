fc557ffd58d79ac9e82f692982a6a8c1439bf954
vijay <vijay951mane@gmail.com>
2026-01-10 17:59:43 +0530

    update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -13,18 +13,42 @@ exports.register = async (req, res) => {
     }
 };
 
+// exports.login = async (req, res) => {
+//     try {
+//         const data = await authService.login(req.body);
+//         res.status(200).json({
+//             status: 200,
+//             message: 'Login successful',
+//             data
+//         });
+//     } catch (err) {
+//         res.status(400).json({ message: err.message });
+//     }
+// };
 exports.login = async (req, res) => {
     try {
         const data = await authService.login(req.body);
+
+        let message = 'Login successful';
+
+        if (data.role === 1) {
+            message = 'Admin login successfully';
+        } else if (data.role === 2) {
+            message = 'User login successfully';
+        } else if (data.role === 3) {
+            message = 'Client login successfully';
+        }
+
         res.status(200).json({
             status: 200,
-            message: 'Login successful',
+            message,
             data
         });
     } catch (err) {
         res.status(400).json({ message: err.message });
     }
 };
+
 
 exports.forgotPassword = async (req, res) => {
     try {
